2c0f917bb196d79a817a758a4c11a0cecaa952cd
mezeigergely <mezei.gergely89@gmail.com>
2023-01-31 17:00:40 +0100

    added users, holidays migration file

---
+++ database/migrations/2023_01_31_155200_create_holidays_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHolidaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('h_start_date');
+            $table->string('h_end_date');
+            $table->string('h_type');
+            $table->integer('h_max')->default(25);
+            $table->integer('h_remaining');
+            $table->boolean('h_is_active')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('holidays');
+    }
+}
